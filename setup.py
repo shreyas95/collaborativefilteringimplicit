d6278c505b7c9c0cb12d28a07ac3ccbbb2337ec2
Shreyas Pansare <sspansare95@gmail.com>
2018-04-23 18:36:06 -0500

    updating folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='collaborativefilteringimplicit',
+      version='0.1',
+      description='Collaborative filtering on implicit data can be done using this module. Confidence/Preference framework is implimented using stochastic gradient descent. "Expected Percentile Rank" is the evaluation metric.',
+      url='https://github.com/shreyas95/collaborativefilteringimplicit',
+      author='Shreyas Pansare',
+      author_email='sspansare95@gmail.com',
+      license='MIT',
+      packages=['collaborativefilteringimplicit'],
+      zip_safe=False)
